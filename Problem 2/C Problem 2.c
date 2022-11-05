a02b766b83e0b6df546d78b2b15362e1b44b89ae
NafisMahmudAyon <97181505+NafisMahmudAyon@users.noreply.github.com>
2022-11-05 20:30:26 +0600

    Create C Problem 2.c

---
+++ Problem 2/C Problem 2.c
@@ -0,0 +1,19 @@
+// Problem 2:
+// QUE: C Program To Print ASCII Value Of Character.
+
+// Source Code:
+
+#include<stdio.h>
+
+main()
+{
+   // Follow US on =>> [ C Programming ]
+  
+   char a;
+   int b;
+   printf("Enter The Character ");
+   scanf("%c",&a);
+   b=a;
+   printf("\nOutput Is =%d ",b);
+
+}
